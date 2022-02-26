b2bf800559f891ae1cc823c33b15a22de4b181f9
John U. Amah <84844009+foresightJ@users.noreply.github.com>
2022-02-26 17:48:53 -0500

    switched to CBV for CRUD

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from .views import ViewListView
 
 urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
-  path('views/', views.views_index, name='about'),
+  # path('views/', views.views_index, name='index'),
+  path('views/', ViewListView.as_view(), name='views_index'),
   path('views/<int:view_id>/', views.views_detail, name='detail')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,18 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import View
-# class View:
-#     def __init__(self, name, location, occassion, credit):
-#         self.name = name
-#         self.location = location
-#         self.occassion = occassion
-#         self.credit = credit
 
-# views = [
-#     View('salt sunset', 'mamaia', 'a calm walk by the beach', 'Foresight'),
-#     View('Lolo', 'tabby', 'foul little demon', 'Me'),
-#     View('Sachi', 'tortoise shell', 'diluted tortoise shell', 'Me'),
-#     View('Raven', 'black tripod', '3 legged cat', 'Bella')
-# ]
+class ViewListView(ListView):
+    model = View
+    template_name = 'views/index.html'
+    
 
 def home(request):
     return render(request, 'home.html')
@@ -20,9 +13,9 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 
-def views_index(request):
-    views = View.objects.all()
-    return render(request, 'views/index.html', {'views': views })
+# def views_index(request):
+#     views = View.objects.all()
+#     return render(request, 'views/index.html', {'views': views })
 
 def views_detail(request, view_id):
     view = View.objects.get(id=view_id)
